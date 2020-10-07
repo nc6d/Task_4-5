f8ec8767048efe634b68c09fe5aca573dfc9fc4d
nc6d <6od92002@gmail.com>
2020-10-07 14:07:36 +0300

    Task 5 don't ready at all

---
+++ Task4-5/Task_5/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Collections.Generic;
+
+
+namespace Task_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string Path1 = @"C:\Users\nc_6d\source\repos\task4&5\Task4-5\x.txt";
+            //string Path2 = @"C:\Users\nc_6d\source\repos\task4&5\Task4-5\y.txt";
+
+            //using StreamReader sr1 = new StreamReader(Path1, Encoding.UTF8);
+            //using StreamReader sr2 = new StreamReader(Path2, Encoding.UTF8);
+
+            int[] arr_x = { 43, 56, 15, 65, 34 };
+            int[] arr_y = { 5, 6, 7, 8, 9 };
+            List<int> arr_z = new List<int>();
+
+            for (int i = 0; i < arr_x.Length; i++)
+            {
+                if (arr_x[i] % 5 == 0)
+                {
+                    arr_x[i] -= 8;
+                    arr_z.Add(arr_x[i]);
+                    
+                }
+                Console.WriteLine(arr_z);
+            }
+            
+
+            
+
+
+
+
+
+
+
+        }
+
+    }
+}
---
+++ Task4-5/test4/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+
+namespace test4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Clear();
+            double radius;
+            do
+            {
+                Console.Write("Enter radius of circle: ");
+                if (!double.TryParse(Console.ReadLine(), out radius) || radius <= 0)
+                {
+                    Console.WriteLine("Invalid radius, try again");
+                }
+            }
+            while (radius <= 0);
+
+            double circleSquare = CalcualtingSquare(radius);
+            double circleLength = CalculatingLength(radius);
+
+            Console.WriteLine($" Radius: {radius}\n Square: {circleSquare}\n Length {circleLength}");
+
+            CircleDraw(radius);
+
+
+        }
+        static double CalcualtingSquare(double radius)
+        {
+            double square = radius * radius * Math.PI;
+            return square;
+        }
+
+        static double CalculatingLength(double radius)
+        {
+            double length = 2 * Math.PI * radius;
+            return length;
+        }
+
+        static void CircleDraw(double radius)
+        {
+            bool toFillOrNot = false;
+            string input;
+
+            do
+            {
+                Console.Write("Do you want to fill the circle  y/n : ");
+                input = Console.ReadLine().ToLower();
+                if (input != "y" && input != "n")
+                {
+                    Console.WriteLine("Invalid reply, try again");
+                }
+                else if (input == "y")
+                {
+                    toFillOrNot = true;
+                }
+            }
+            while (input != "y" && input != "n");
+
+            Console.WriteLine();
+
+            double radiusIn = radius - 0.4;
+            double radiusOut = radius + 0.4;
+
+            for (double y = radius; y >= -radius; --y)
+            {
+                for (double x = -radius; x < radiusOut; x += 0.5)
+                {
+
+                    double value = x * x + y * y;
+
+                    if (value >= radiusIn * radiusIn && value <= radiusOut * radiusOut)
+                    {
+                        Console.Write(".");
+                    }
+                    else if (toFillOrNot && value < radiusIn * radiusIn && value < radiusOut * radiusOut)
+                    {
+                        Console.Write(".");
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+                } Console.WriteLine();
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
